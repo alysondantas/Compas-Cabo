5301c75576c874652e16d71b503ae62ffcbe5e61
alysondantas <alyson.lipe@gmail.com>
2016-05-09 20:18:54 -0300

    Revert "mudando um pouco"
    This reverts commit 6a192b99d2839305c1d074a75eb4dcdb9f30de3e.

--- src/br/uefs/ecomp/comprascabo/controller/Controller.java
+++ src/br/uefs/ecomp/comprascabo/controller/Controller.java
@@ -28,8 +28,9 @@ public class Controller {
 		vendas = new Lista();
 	}
 	public void escreverTudoArquivo() throws IOException { //método deixado aqui só para inspiração. Não vai ser usado do jeito que está descrito no momento
-		File f = new File("Produto.txt");
-		BufferedWriter writer = new BufferedWriter(new FileWriter(f, true));
+		URL path = Controller.class.getResource("Produto.txt");
+		File f = new File(path.getFile());
+		BufferedWriter writer = new BufferedWriter(new FileWriter(f));
 		String texto = "";
 		MeuIterador iterador=(MeuIterador) produtos.iterador();
 		Produto produto;
